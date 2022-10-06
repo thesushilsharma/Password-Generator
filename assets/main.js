1d235e8eae22b6abc0970cda6f90016fc4de07a9
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2022-10-06 11:18:44 +0400

    coding progress

---
+++ assets/main.js
@@ -0,0 +1,7 @@
+const password_ele = document.getElementById("pwd_txt");
+const LOWERCASE_CODES = "acdefghijklnopqrstuvwxyz";
+const UPPERCASE_CODES = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const NUMBER_CODES = "0123456789";
+const SYMBOL_CODES = "@#$%^&*";
+
+
