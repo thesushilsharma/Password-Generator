f2e1b57c9215a001a289804b348e22b1ac677da0
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2022-10-31 01:42:23 +0400

    fucntion generateRandomPassword

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,13 @@
 const password_ele = document.getElementById("pwd_txt");
-const LOWERCASE_CODES = "acdefghijklnopqrstuvwxyz";
-const UPPERCASE_CODES = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const NUMBER_CODES = "0123456789";
-const SYMBOL_CODES = "@#$%^&*";
+const LOWERCASE_CODES = 'acdefghijklnopqrstuvwxyz';
+const UPPERCASE_CODES = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const NUMBER_CODES = '0123456789';
+const SYMBOL_CODES = '~`!@#$%^&*()_+-={}[]:";\'<>?,./|\\';
 
 
-function generatePassword()
-{
-    
+function generateRandomPassword(length, chars) {
+    var result = '';
+    for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+
+    return result;
 }
-
-
