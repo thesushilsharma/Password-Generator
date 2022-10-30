4d0ec47a685e71559d82d13ac0c1ea8366d6d490
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2022-10-31 01:49:50 +0400

    working

--- assets/main.js
+++ assets/main.js
@@ -3,17 +3,15 @@ const UPPERCASE_CODES = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const NUMBER_CODES = '0123456789';
 const SYMBOL_CODES = '~`!@#$%^&*()_+-={}[]:";\'<>?,./|\\';
 
-
 function password() {
     const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     const length = 6;
-    var result = generatePassword(length, chars);
+    var result = generateRandomPassword(length, chars);
     document.getElementById('pwd_txt').innerHTML = result;
 }
 
 function generateRandomPassword(length, chars) {
     var result = '';
     for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
-
     return result;
 }
