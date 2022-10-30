7ad2b99886f03df2bbb0c895497edfe08e126dfa
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2022-10-31 02:23:31 +0400

    adding symbols

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,10 @@
-const LOWERCASE_CODES = 'acdefghijklnopqrstuvwxyz';
-const UPPERCASE_CODES = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const NUMBER_CODES = '0123456789';
-const SYMBOL_CODES = '~`!@#$%^&*()_+-={}[]:";\'<>?,./|\\';
-
 function password() {
-    const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    const LOWERCASE_CODES = 'acdefghijklnopqrstuvwxyz';
+    const UPPERCASE_CODES = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const NUMBER_CODES = '0123456789';
+    const SYMBOL_CODES = '~`!@#$%^&*()_+-={}[]:";\'<>?,./|\\';
+    const chars = UPPERCASE_CODES + LOWERCASE_CODES + NUMBER_CODES + SYMBOL_CODES;
     const length = 6;
     var result = generateRandomPassword(length, chars);
     document.getElementById('pwd_txt').innerHTML = result;
